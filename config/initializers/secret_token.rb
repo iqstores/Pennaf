9b9fed4a7882f4476f35c523f7afefb960595818
unknown <IQstores@.(none)>
2010-09-11 23:33:20 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pennaf::Application.config.secret_token = '4fa1a829637aeedaee46ed5cdfce0a3056bc3704ed106f517ab43af869bf18fe6554ed6fcda9f206a73bddbdff366d49d94a1d48583b60c77ef058f9f7bec9a3'
